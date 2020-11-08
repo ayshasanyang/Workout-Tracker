f11bbcdaf332136029ff90a4cb1e96bf3d7e47a6
Isatou D Sanyang <isasanyang1986@yahoo.co.uk>
2020-11-07 18:12:33 -0600

    adding to workout

--- models/workout.js
+++ models/workout.js
@@ -33,13 +33,26 @@ const WorkoutSchema = new Schema({
       }
     }
   ]
-});
-
-WorkoutSchema.virtual("totalDuration").get(function() {
+}, 
+{
+  toJSON: {
+    virtuals: true
+  }
+}
+);
+WorkoutSchema.virtual("totalDuration").get(function () {
   return this.exercises.reduce((total, exercise) => {
     return total + exercise.duration;
   }, 0);
 });
+WorkoutSchema.virtual("totalDistance").get(function() {
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.distance;
+    }, 0);
+});
+
+
+
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
